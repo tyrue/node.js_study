19c5fe7afc36c2138461031281e9665777e3e743
tyrue <tyrue2@gmail.com>
2019-05-02 11:34:03 +0900

    ss

--- main.js
+++ main.js
@@ -8,16 +8,25 @@ var FileStore = require('session-file-store')(session) // npm i -s session-file-
 const express = require('express'); // 상수화
 const app = express(); // 상수화
 
-app.use(express.urlencoded({ extended: false })); // bodyparser
+app.use(express.urlencoded({
+    extended: false
+})); // bodyparser
 app.use(session({
     secret: 'keyboard cat', // 필수 옵션, 
-    resave: false,  // 값이 변하기 전까지는 새로 저장 안함
+    resave: false, // 값이 변하기 전까지는 새로 저장 안함
     saveUninitialized: true, // 세션이 필요하기 전까지 구동하지 않음
     store: new FileStore() // 파일 저장, 코드가 있는 디렉토리에 session 폴더를 만들고 거기에 저장함
 }));
 app.use('/', topicRouter);
 app.use('/', authorRouter);
 
+var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy;
+app.post('/login',
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+    }));
 
 // requst : 요청할 때 보내는 정보
 // response : 응답할 때 보내는 정보
